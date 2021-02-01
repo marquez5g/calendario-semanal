816acbb064b28e633080437e00683601a21abaa3
pantraz <marquez122@outlook.com>
2021-02-01 10:23:06 -0500

    cambio color titulo

--- src/App.vue
+++ src/App.vue
@@ -33,26 +33,7 @@ export default {
   font-family: "Courier New", Courier, monospace;
   font-weight: bold;
   text-transform: uppercase;
-  color: #2b2929;
+  color: #ffffff;
   margin: 0;
 }
-/* .header {
-  position: fixed; 
-  top: 0px;
-  left: 0;
-  overflow:auto;
-  width:100%;
-  overflow:hidden; 
-  height:70px;
-  background: #aa9b9b;
-  z-index: 1;
-  opacity: 80%;
-   
-  
-} */
-/* .main-component{
-  position: initial;
-  margin-top: 100px;
-  z-index: -1;
-} */
 </style>
